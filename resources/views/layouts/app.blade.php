fd0c8ab5c6449b082bc589f0dacab314955acde6
Lesly Perez <lkpsmx@gmail.com>
2021-12-16 17:18:57 -0600

    chore: style and font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -86,6 +79,7 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 
 </html>
